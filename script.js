cdcb65b39ecceddf661f060f6aa230784fdbc177
Mayank jaiswal <mayank.jazz21@gmail.com>
2023-03-06 17:39:05 +0530

    second commit

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-var time= prompt("what is the time: ");
-if(time > 5 && time < 12){
-    alert("good Morning");
+// var time= prompt("what is the time:" );
+// if(time > 5 && time < 12){
+//     alert("good Morning");
+// }
+// else if( time > 12 && time < 17){
+//     alert("good afternoon");
+// }
+// else if(time >17 && time < 21){
+//     alert("good evening");
+// }
+// else{
+//     alert("good night");
+// }
+
+var fruitType= prompt("kaun sa faal chaaiye??")
+
+switch(fruitType){
+    case 'Narangi':
+        console.log('Narangi hai 60rs kilo.');
+        break;
+    case 'sabe':
+        console.log('sabe hai 180rs kilo.');
+        break;
+    case 'kele':
+      console.log('kele hai 35rs kilo.');
+       break;
+     case 'Amrudh':
+      console.log('Amrudh hai 80rs kilo.');
+       break;
+    case 'Aam':
+      console.log('Aam hai 300rs kilo.');
+       break;
+    case 'papita':
+      console.log('papita hai 200rs kilo.');
+       break;
+    default:
+        console.log(`${fruitType} ajj khtm ho gya kal aana`)
 }
-else if( time > 12 && time < 17){
-    alert("good afternoon");
-}
-else if(time >17 && time < 21){
-    alert("good evening");
-}
-else{
-    alert("good night");
-}
+console.log("orr kuch chaiye??");
